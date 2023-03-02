0d5176ac597009cebfcd908b37a5d8eafe6d82e5
Mateo Estrada <mateoej12@gmail.com>
2023-03-01 23:39:16 -0800

    Finished three of the requirements.

--- AES.js
+++ AES.js
@@ -3,24 +3,45 @@
 // This is the file that will have all the AES functions.
 // It leverages the crypto-js library to do the heavy lifting.
 
+const { generateKey } = require('crypto');
+const crypto = require('crypto');
 // Reads a local json file and returns the data
 function readJSONFile(file) {
     // Read the file using the file system
     const fs = require('fs');
-    let rawData = fs.readFileSync('message.json');
+    let rawData = fs.readFileSync(file);
     let messages = JSON.parse(rawData);
-    
-    return messages;
+    return JSON.stringify(messages);
+}
+
+function makeKey() {
+    return crypto.randomBytes(32);
+}
+
+function Encrypt(message, key) {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    let encrypted = cipher.update(message, 'utf-8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
 }
 
 
 // Tests the readJSONFile function
 function testReadJSONFile() {
-    var file = readJSONFile("AES.json");
+    let file = readJSONFile("message.json");
     console.log("The file is running", file);
 }
 
+function testMakeKey() {
+    let key = makeKey();
+    console.log("The key is", key);
+}
 
+let encMsg = Encrypt(readJSONFile("message.json"), crypto.randomBytes(32));
+console.log("The encrypted message is:", encMsg);
+
+
+//* TESTS *//
 testReadJSONFile();
-
-   
+testMakeKey();
