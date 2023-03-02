c3875f64a20bee6af3246d0f710a701a00348a57
Mateo Estrada <mateoej12@gmail.com>
2023-03-01 22:29:11 -0800

    Adding the readJSONFile function.

--- AES.js
+++ AES.js
@@ -1,3 +1,26 @@
 // Mateo Estrada Jorge
 // March 1, 2023
 // This is the file that will have all the AES functions.
+// It leverages the crypto-js library to do the heavy lifting.
+
+// Reads a local json file and returns the data
+function readJSONFile(file) {
+    // Read the file using the file system
+    const fs = require('fs');
+    let rawData = fs.readFileSync('message.json');
+    let messages = JSON.parse(rawData);
+    
+    return messages;
+}
+
+
+// Tests the readJSONFile function
+function testReadJSONFile() {
+    var file = readJSONFile("AES.json");
+    console.log("The file is running", file);
+}
+
+
+testReadJSONFile();
+
+   
